31628345799d7c769fedd559b77e7fcf4779f2f9
Amanda Slayton <alslayton86@gmail.com>
2018-10-16 19:51:29 -0500

    complete num6

---
+++ ChinookQuery6.sql
@@ -0,0 +1,16 @@
+use Chinook
+
+/*
+Provide a query that shows the invoices associated with each sales 
+agent. The resultant table should include the Sales Agent's full 
+name.
+*/
+
+select 
+  FullName = e.FirstName + ' ' + e.LastName,
+  i.InvoiceId
+from Customer c
+join Employee e
+on c.SupportRepId = e.EmployeeId
+join Invoice i
+on c.CustomerId = i.CustomerId
