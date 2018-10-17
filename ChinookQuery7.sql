7cb30ff134003dfc0183bb3170d458ddbcfd4c15
Amanda Slayton <alslayton86@gmail.com>
2018-10-16 20:16:02 -0500

    complete num7

---
+++ ChinookQuery7.sql
@@ -0,0 +1,15 @@
+use Chinook
+
+/*Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.*/
+
+select 
+  InvoiceTotal = i.Total,
+  CustomerName = c.FirstName + ' ' + c.LastName,
+  c.Country,
+  AgentName = e.FirstName + ' ' + e.LastName
+from Customer c
+join Invoice i
+on c.CustomerId = i.CustomerId
+join Employee e
+on c.SupportRepId = e.EmployeeId
+order by c.CustomerId
