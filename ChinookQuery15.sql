f13aaa787767fa0010d3f718abe9a0a31d91a927
Amanda Slayton <alslayton86@gmail.com>
2018-10-20 09:17:27 -0500

    finished 15

---
+++ ChinookQuery15.sql
@@ -0,0 +1,13 @@
+use Chinook
+
+/*
+Provide a query that shows the total number of tracks in each playlist. 
+The Playlist name should be include on the resulant table.
+*/
+
+select
+  PlaylistId as PlaylistId, 
+  count(TrackId) as NumOfTracks
+from PlaylistTrack
+GROUP by PlaylistId
+
