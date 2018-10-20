9143ebb0e9c1a36a76e708390befbf9088fc3f1d
Amanda Slayton <alslayton86@gmail.com>
2018-10-20 10:57:34 -0500

    finish num23

---
+++ ChinookQuery23.sql
@@ -0,0 +1,11 @@
+use Chinook
+
+/*
+Which country's customers spent the most?
+*/
+
+select top 1
+  BillingCountry, sum(Total) as TotalSales
+from Invoice
+Group by BillingCountry
+Order By TotalSales desc
